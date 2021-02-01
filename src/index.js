4b984d020024e408626bf42cf49f30acc474dd6b
Paul Iambaev <slushau.vnimatelno@yandex.ru>
2021-02-01 20:10:40 +0500

    complete exercise

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
+exports.min = function min(array) {
+    if (!isCorrectArray(array)) {
+        return 0;
+    }
 
-exports.min = function min (array) {
-  return 0;
-}
+    let min = Infinity;
 
-exports.max = function max (array) {
-  return 0;
-}
+    for (const num of array) {
+        min = Math.min(min, num);
+    }
 
-exports.avg = function avg (array) {
-  return 0;
+    return min;
+};
+
+exports.max = function max(array) {
+    if (!isCorrectArray(array)) {
+        return 0;
+    }
+    let max = -Infinity;
+
+    for (const num of array) {
+        max = Math.max(max, num);
+    }
+
+    return max;
+};
+
+exports.avg = function avg(array) {
+    if (!isCorrectArray(array)) {
+        return 0;
+    }
+
+    let avg = 0;
+
+    for (const num of array) {
+        avg += num;
+    }
+
+    return avg / array.length;
+};
+
+function isCorrectArray(arr) {
+    return Array.isArray(arr) && arr.length !== 0;
 }
